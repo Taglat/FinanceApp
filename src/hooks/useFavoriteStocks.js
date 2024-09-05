1dc638f85d4bd764f04bf27f7fa47882080c6e2b
Talgat Tokayev <ttokayev7@gmail.com>
2024-09-05 21:05:29 +0500

    custom hooks

---
+++ src/hooks/useFavoriteStocks.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { addSymbolToFavorites } from "../helpers/addSymbolToFavorites";
+import { deleteSymbolFromFavorites } from "../helpers/deleteSymbolFromFavorites";
+
+export function useFavoriteStocks() {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const storage = localStorage.getItem("favorites");
+    if (storage) {
+      setFavorites(JSON.parse(storage));
+    }
+  }, []);
+
+  const addSToFavorites = (symbol) => {
+    addSymbolToFavorites(symbol, favorites, () =>
+      setFavorites((prev) => [symbol, ...prev])
+    );
+  };
+
+  const deleteFromFavorites = (symbol) => {
+    deleteSymbolFromFavorites(symbol, (newArray) => setFavorites(newArray));
+  };
+
+  return {
+    favorites,
+    addSToFavorites,
+    deleteFromFavorites
+  }
+}
--- src/pages/Stocks/Stocks.jsx
+++ src/pages/Stocks/Stocks.jsx
@@ -1,29 +1,10 @@
-import { useEffect, useState } from "react";
 import SearchStocks from "../../components/SearchStocks/SearchStocks";
 import StocksList from "../../components/StocksList/StocksList";
-import { addSymbolToFavorites } from "../../helpers/addSymbolToFavorites";
-import { deleteSymbolFromFavorites } from "../../helpers/deleteSymbolFromFavorites";
 import style from "./style.module.css";
+import { useFavoriteStocks } from "../../hooks/useFavoriteStocks";
 
 const Stocks = () => {
-  const [favorites, setFavorites] = useState([]);
-
-  useEffect(() => {
-    const storage = localStorage.getItem("favorites");
-    if (storage) {
-      setFavorites(JSON.parse(storage));
-    }
-  }, []);
-
-  const addSToFavorites = (symbol) => {
-    addSymbolToFavorites(symbol, favorites, () =>
-      setFavorites((prev) => [symbol, ...prev])
-    );
-  };
-
-  const deleteFromFavorites = (symbol) => {
-    deleteSymbolFromFavorites(symbol, (newArray) => setFavorites(newArray));
-  };
+  const { favorites, addSToFavorites, deleteFromFavorites } = useFavoriteStocks();
 
   return (
     <div className={style.stocks}>
